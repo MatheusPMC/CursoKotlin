68032d1217c4910b19a7d8d744d2732deaac886d
MatheusPMC <matheus.pmc@hotmail.com>
2021-09-21 20:12:05 -0300

    Fundamentos01

---
+++ src/main/kotlin/fundamentos/Comentarios.kt
@@ -0,0 +1,16 @@
+package fundamentos
+
+/**
+ * Só para dizer que kotlin suporta o KDoc... :)
+ *
+ * @param args lista de parâmetros passados por linha de comando
+ */
+fun main() {
+    // Comentário de uma linha
+
+    /*
+    mais de
+    uma linha
+     */
+
+}
---
+++ src/main/kotlin/fundamentos/TiposBasicos.kt
@@ -0,0 +1,19 @@
+package fundamentos
+
+fun main() {
+    //Tipos Numericos Inteiros
+    val num1: Byte = 127
+    val num2: Short = 32767
+    val num3: Int = 2_147_483_647
+    val num4: Long = 2_123_123_123_123 // Long.MAX_VALUE
+
+    //Tipos Numericos Reais
+    val num5: Float = 3.14F
+    val num6: Double = 3.14
+
+    //Tipo Caractere
+    val char: Char = '?'
+
+    // Tipo booleano
+    val boolean: Boolean = true // ou false
+}
