b41d3ee21cd5c89df6539e97a855cb9dd7bbaab6
MatheusPMC <matheus.pmc@hotmail.com>
2021-10-03 19:36:13 -0300

    Funcoes

---
+++ src/main/kotlin/funcoes/ParametrosImutaveis.kt
@@ -0,0 +1,9 @@
+package funcoes
+
+fun incremento(num: Int) {
+  //  num++ // num = num + 1 || embora não esteja explicito no codigo, eles é uma const e não pode atribuir o valor
+}
+
+fun main() {
+    incremento(3)
+}
